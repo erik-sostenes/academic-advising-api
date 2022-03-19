9acd6b679053ac2a004e5e9fd61fe5c66e977008
Erik Sostenes Simon <eriksostenessimon07@gmail.com>
2022-03-19 17:37:57 -0600

    Load MySQL connection
    issue #1

--- internal/repository/connection_sql.go
+++ internal/repository/connection_sql.go
@@ -1,1 +1,48 @@
 package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"sync"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	once sync.Once
+)
+
+func LoadSqlConnection() (*sql.DB, error) {
+	var err error
+	var sqlConnection *sql.DB
+
+	once.Do(func() {
+		driverName := "mysql"
+
+		url := fmt.Sprintf(
+			"%s:%s@tcp(%s:%s)/%s",
+			"root",
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_HOST"),
+			"3306",
+			"advices",
+		)
+		sqlConnection, err = sql.Open(driverName, url)
+		if err != nil {
+			return
+		}
+		err = sqlConnection.Ping()
+	})
+	return sqlConnection, err
+}
+
+func NewDB() *sql.DB {
+	db, err := LoadSqlConnection()
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+
