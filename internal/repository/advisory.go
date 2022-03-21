9d3f4c763eab012f514686be5b4a4130729f7b2b
Erik Sostenes Simon <eriksostenessimon07@gmail.com>
2022-03-20 23:01:13 -0600

    Implement the functionality of adding new academic advising the AddAdvisory method.
    issue #2

---
+++ internal/repository/advisory.go
@@ -0,0 +1,65 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/itsoeh/academic-advising-api/internal/model"
+)
+
+// AdvisoryAggregator contains the method to add a new academic advisory
+type AdvisoryAggregator interface {
+	// AddAdvisory method that has the task of adding a new academic advisory through a sql query
+	AddAdvisory(model.AcademicAdvisory) error
+}
+
+// advisoryAggregator implements AdvisoryAggregator interface 
+type advisoryAggregator struct {
+	*sql.DB
+}
+
+func NewAdvisoryAggregator() advisoryAggregator {
+	return advisoryAggregator{
+		DB: NewDB(),
+	}
+}
+
+func (a *advisoryAggregator) AddAdvisory(advisory model.AcademicAdvisory) (err error) {
+	_, err = a.Exec(sqlQueryAddAdvisory,
+									advisory.AdvisoryId,
+									advisory.Description,
+									advisory.Reports,
+									advisory.FromDate,
+									advisory.ToDate,
+									advisory.IsActive,
+									advisory.AcademicAdvisoryIds.SubjectId,
+									advisory.AcademicAdvisoryIds.StudentTuition,
+									advisory.AcademicAdvisoryIds.TeacherTuition,
+									advisory.AcademicAdvisoryIds.UniversityCourseId,
+									advisory.AcademicAdvisoryIds.SubCoordinatorTuition,
+									advisory.AcademicAdvisoryIds.CoordinatorTuition,
+								)
+
+	if code, ok := err.(*mysql.MySQLError); ok {
+		log.Printf("Error add advisory: %v", err.Error())
+
+		//NOTE: Error Code: 1062. Duplicate entry "advisory_id" for key 
+		//NOTE: Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails 
+
+		if code.Number == 1062 {
+			err = model.StatusBadRequest(fmt.Sprintf("The advisory whith id %v already exist", advisory.AdvisoryId))
+			return
+		} 
+		
+		if code.Number == 1452 {
+			err = model.StatusBadRequest("Check that all information fields of the advisory are correct")	
+			return
+		}
+
+		err = model.InternalServerError("An error has occurred when adding a new advisory")
+		return 
+	}
+	return
+}
