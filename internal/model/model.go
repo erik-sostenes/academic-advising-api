c8fc53d4aa4787b80767d82c44b7881093b75b9d
erik-sostenes-simon <eriksostenessimon@gmail.com>
2022-05-02 03:01:23 -0500

    Refactor the structure AcademicAdvisor and AcademicAdvisoryIds
    issue #15

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,25 +1,23 @@
 package model
 
-import "time"
-
 // Models
 type (
 	// AcademicAdvisoryIds conatins all the ids that make up the academic advisory
 	AcademicAdvisoryIds struct {
-		SubjectId             uint32 `json:"subject_id"`
-		StudentTuition        uint32 `json:"student_tuition"`
-		TeacherTuition        uint32 `json:"teacher_tuition"`
-		UniversityCourseId    uint32 `json:"university_course_id"`
-		SubCoordinatorTuition uint32 `json:"sub_coordinator_tuition"`
-		CoordinatorTuition    uint32 `json:"coordinator_tuition"`
+		SubjectId             string `json:"subject_id"`
+		StudentTuition        string `json:"student_tuition"`
+		TeacherTuition        string `json:"teacher_tuition"`
+		UniversityCourseId    string `json:"university_course_id"`
+		SubCoordinatorTuition string `json:"sub_coordinator_tuition"`
+		CoordinatorTuition    string `json:"coordinator_tuition"`
 	}
 	// AcademicAdvisory contains the fields of the academic advisory
 	AcademicAdvisory struct {
 		AdvisoryId          string              `json:"advisory_id"`
 		Description         string              `json:"description"`
 		Reports             string              `json:"reports,omitempty"`
-		FromDate            time.Time           `json:"from_date"`
-		ToDate              time.Time           `json:"to_date,omitempty"`
+		FromDate            string              `json:"from_date"`
+		ToDate              string              `json:"to_date,omitempty"`
 		RecordTime          uint32              `json:"record_time,omitempty"`
 		IsActive            bool                `json:"is_active"`
 		IsAccepted          bool                `json:"is_accepted"`
